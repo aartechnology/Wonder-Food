631051a3c9f657c126e570f1d73e4db2428e117f
aartechnology <69177973+aartechnology@users.noreply.github.com>
2020-08-05 19:05:53 +0600

    Add files via upload

---
+++ register.php
@@ -0,0 +1,35 @@
+<?php
+	$email = '';
+	if (isset($_GET['email'])){
+		$email = $_GET['email'];
+	}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register | Wonder Food</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/login-form.css">
+</head>
+<body>
+	<div class="overlay">
+		
+	</div>
+	<div class="login">
+		<h3>Rgister in Wonder Food</h3>
+		<div class="field-box">
+			<input type="text" name="name" placeholder="Full Name...">
+			<input type="email" name="email" placeholder="Email..." value="<?php echo $email ?>">
+			<input type="username" name="username" placeholder="Username...">
+			<input type="password" name="cpass" placeholder="Create Password...">
+			<input type="password" name="ccpass" placeholder="Confirm Password...">
+			<div class="options">
+				<button type="button">Register</button>
+				<a href="login.html">Have an account?</a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
